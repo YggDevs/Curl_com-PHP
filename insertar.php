9d0daf956201b46337ca659504a37d9b48d6e8ca
YggDevs <43205417+YggDevs@users.noreply.github.com>
2023-06-01 01:03:09 +0200

    Update insertar.php
      Added comments to explain the purpose of each section and form.
       Added form-group class to the Name and Email inputs for consistent styling.
       Updated the form action URLs to match the correct endpoints ("/insertar.php", "/borrar.php", and "/vertablas.php").
       Added a label for each input field for better accessibility.
       Added Bootstrap classes to the submit button for consistent styling.
        Removed unnecessary error_reporting lines.

--- insertar.php
+++ insertar.php
@@ -1,5 +1,5 @@
+<!DOCTYPE html>
 <html>
-<title>index.html</title>
 <head>
   <title>App</title>
   <meta charset="utf-8">
@@ -11,82 +11,86 @@
 </head>
 <body>
 
-
 <div class="jumbotron text-center">
-  <h1>Consume spring boot </h1>
-  <p>Pagina responsive hecha con PHP Curl </p> 
+  <h1>Consume Spring Boot</h1>
+  <p>Pagina responsive hecha con PHP Curl</p> 
 </div>
-  
 
 <div class="container">
-
-
-<form action="/insertar.php">
+  <!-- Insertar Form -->
+  <form action="/insertar.php">
     <div class="row">
-    <div class="col-sm-6" >
-          <button  type="submit" class="btn btn-primary">Insertar</button>
+      <div class="col-sm-6">
+        <button type="submit" class="btn btn-primary">Insertar</button>
+      </div>
     </div>
-</form>
+  </form>
 
-<form action="/borrar.php">
-    <div class="col-sm-6">
-          <button type="submit" class="btn btn-primary">Borrar</button>
-    </div>
-</form>
-
-
-<form action="/vertablas.php">
-    <div class="col-sm-6">
-          <button type="submit" class="btn btn-primary">Ver tablas</button>
-    </div>
-</form>
-    </div>
-
- 
-    <div class="container">
-                        <h2>Panel de control</h2>
-    </div>
-    <div class="container">
+  <!-- Borrar Form -->
+  <form action="/borrar.php">
     <div class="row">
+      <div class="col-sm-6">
+        <button type="submit" class="btn btn-primary">Borrar</button>
+      </div>
+    </div>
+  </form>
+
+  <!-- Ver tablas Form -->
+  <form action="/vertablas.php">
+    <div class="row">
+      <div class="col-sm-6">
+        <button type="submit" class="btn btn-primary">Ver tablas</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+<div class="container">
+  <h2>Panel de control</h2>
+</div>
+
+<div class="container">
+  <div class="row">
     <div class="col-sm-4">
-                          <h3>Insertar</h3>
-   
-<form  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-      Name: <input type="text" name="name">
-      <br><br>
-      E-mail: <input type="text" name="email">
-      <br><br>
-            <input type="submit" name="submit" value="Insertar">  
-</form>
+      <h3>Insertar</h3>
+      <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <!-- Name input -->
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" class="form-control" name="name" id="name">
+        </div>
+
+        <!-- Email input -->
+        <div class="form-group">
+          <label for="email">E-mail:</label>
+          <input type="text" class="form-control" name="email" id="email">
+        </div>
+
+        <input type="submit" name="submit" value="Insertar" class="btn btn-primary">  
+      </form>
     </div>
+  </div>
+</div>
 
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Grabar datos
+  $postData = array(
+    "name" => $_POST["name"],
+    "email" => $_POST["email"]
+  );
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-error_reporting(0);
-error_reporting(E_ERROR);
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/demo/add");
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $data = curl_exec($ch);
+  curl_close($ch);
 
-//grabar datos
- $postData = array( 
-   "name"=>$_POST["name"],
-   "email"=>$_POST["email"]
-
-   
-  );  
-  $ch = curl_init();  
-  curl_setopt($ch, CURLOPT_URL, "localhost:8080/demo/add");  
-  curl_setopt($ch, CURLOPT_HEADER, false);  
-  curl_setopt($ch, CURLOPT_POST, true);  
-  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));  
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
-  $data = curl_exec($ch);  
-  //print_r($data);
-    
-  print("Insertado")  ;
-  curl_close($ch);  
-
+  echo "Insertado";
 }
-
-  ?>
+?>
 </body>
 </html>
