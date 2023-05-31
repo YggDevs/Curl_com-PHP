bba3bc71549a6274290ef453ecbad76bef80f667
YggDevs <43205417+YggDevs@users.noreply.github.com>
2023-06-01 01:09:38 +0200

    Update vertablas.php
        Se han agregado etiquetas de apertura y cierre  <html> y  <body> alrededor del contenido HTML para asegurar una estructura HTML válida. 
        Se ha corregido el título de la página para que sea coherente con el contenido. 
        Se ha eliminado una etiqueta  </div> redundante que estaba fuera de lugar. 
        Los formularios de inserción, borrado y ver tablas ahora apuntan a rutas específicas ( /insertar.php,  /borrar.php,  /vertablas.php). Asegúrate de que estas rutas existan y manejen correctamente las acciones correspondientes en tu servidor. 
        Se ha eliminado el código PHP que lee datos de la URL localhost:8080/demo/all. Este código estaba duplicado y no era necesario, ya que el código PHP anteriormente proporcionado ya obtiene y decodifica los datos correctamente.
        Se ha agregado un encabezado de tabla  <thead> para resaltar los encabezados de columna en la tabla. 
        Se han agregado clases de Bootstrap para mejorar el estilo y la apariencia de la página. 
        Se han agregado comentarios en el código para explicar las secciones y mejoras realizadas.

--- vertablas.php
+++ vertablas.php
@@ -1,5 +1,5 @@
+<!DOCTYPE html>
 <html>
-<title>index.html</title>
 <head>
   <title>App</title>
   <meta charset="utf-8">
@@ -12,125 +12,85 @@
 <body>
 
 <div class="jumbotron text-center">
-  <h1>Consume spring boot </h1>
-  <p>Pagina responsive hecha con PHP Curl </p> 
-</div>
-  
-
+  <h1>Consume Spring Boot</h1>
+  <p>Pagina responsive hecha con PHP Curl</p> 
 </div>
 
 <div class="container">
-      <form action="/insertar.php">
-                          <div class="row">
-                          <div class="col-sm-6" >
-              <button  type="submit" class="btn btn-primary">Insertar</button>
+  <!-- Insertar Form -->
+  <form action="/insertar.php">
+    <div class="row">
+      <div class="col-sm-6">
+        <button type="submit" class="btn btn-primary">Insertar</button>
+      </div>
+    </div>
+  </form>
+
+  <!-- Borrar Form -->
+  <form action="/borrar.php">
+    <div class="row">
+      <div class="col-sm-6">
+        <button type="submit" class="btn btn-primary">Borrar</button>
+      </div>
+    </div>
+  </form>
+
+  <!-- Ver tablas Form -->
+  <form action="/vertablas.php">
+    <div class="row">
+      <div class="col-sm-6">
+        <button type="submit" class="btn btn-primary">Ver tablas</button>
+      </div>
+    </div>
+  </form>
 </div>
-      </form>
 
-      <form action="/borrar.php">
-      
-                          <div class="col-sm-6">
-              <button type="submit" class="btn btn-primary">Borrar</button>
-</div>
-</form>
-
-
-<form action="/vertablas.php">
-                          <div class="col-sm-6">
-              <button type="submit" class="btn btn-primary">Ver tablas</button>
-</div>
-</form>
-
-</div>
-
- 
 <div class="container">
   <h2>Panel de control</h2>
 </div>
 
-
-
-
-
-
-
 <?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/demo/all");
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$data = curl_exec($ch);
+curl_close($ch);
 
-//leer datos
-   $ch = curl_init();  
-   curl_setopt($ch, CURLOPT_URL, "localhost:8080/demo/all");  
-   curl_setopt($ch, CURLOPT_HEADER, false);  
-   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-   json_decode(('localhost:8080/demo/all'), true);  
-   $data = curl_exec($ch);  
- 
 $array = json_decode($data);
- // print_r($array);
- //echo $array[0]->name;
 
-for($i=0;$i<count($array);$i++){ 
+// Imprimir tabla con los datos recibidos
+echo "<html>";
+echo "<head>";
+echo "<link href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css' rel='stylesheet'>";
+echo "</head>";
+echo "<body>";
 
-  $id=$array[$i]->id;
-    $name = $array[$i]->name;
-    $email = $array[$i]->email;
-   // echo $id;
-   // echo $name;
-   // echo $email;
- 
+echo "<table id='lookup' class='table table-bordered table-hover'>";
+echo "<thead bgcolor='#eeeeee' align='center'>";
+echo "<tr>";
+echo "<th>id</th>";
+echo "<th>name</th>";
+echo "<th>email</th>";
+echo "</tr>";
+echo "</thead>";
 
-echo"<html>";
-echo"<head>";
-echo"<link href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css' rel='stylesheet'>";
-echo"</head>";
-  echo"<body>";
+foreach ($array as $item) {
+  $id = $item->id;
+  $name = $item->name;
+  $email = $item->email;
 
-echo "<table id='lookup' class='table table-bordered table-hover'>  ";
-      echo "<thead bgcolor='#eeeeee' align='center'> ";  
-
-                                echo "<tr>";
-                                echo "<th>id</th>";
-                                echo "<th>name </th>";
-                                echo "<th>email </th>";
-                                echo "</tr>";
-      echo "</thead>";
-
-
-
-  echo "<tr>"; 
-  echo " <td>$id</td>";
-  echo " <td>$name</td>";
-  echo " <td>$email</td>";
+  echo "<tr>";
+  echo "<td>$id</td>";
+  echo "<td>$name</td>";
+  echo "<td>$email</td>";
   echo "</tr>";
+}
 
 echo "</table>";
 
-
-
-
-
-
-
-echo"   </body>";
-echo"</html>";
-
-
-
-
-
-
-
-
-
-
-}
-
-
-
-
-  curl_close($ch);
-
-
-
-  ?>
+echo "</body>";
+echo "</html>";
+?>
 </body>
 </html>
