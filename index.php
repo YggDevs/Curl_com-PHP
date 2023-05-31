54b718cb61b2517104d50b7e921f7beb4387430b
YggDevs <43205417+YggDevs@users.noreply.github.com>
2023-06-01 01:05:03 +0200

    Update index.php
        Added proper indentation and formatting for better readability.
        Added comments to explain the purpose and functionality of each section.
        Added form labels and Bootstrap form control classes for consistent styling and accessibility.
        Corrected the URL in the curl requests to include "http://" and the correct endpoint URL.
        Moved the PHP code for processing form submissions inside a check for $_SERVER["REQUEST_METHOD"] === "POST", ensuring it executes only when the form is submitted.
        Replaced the deprecated json_decode(('localhost:8080/demo/all'), true); with json_decode($data).
        Removed unnecessary HTML tags (<html>, <head>, and <body>) as they were duplicated.

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <html>
-<title>index.html</title>
 <head>
   <title>App</title>
   <meta charset="utf-8">
@@ -11,222 +10,148 @@
 </head>
 <body>
 
-
 <div class="jumbotron text-center">
-  <h1>Consume spring boot </h1>
-  <p>Pagina responsive hecha con PHP Curl </p> 
+  <h1>Consume Spring Boot</h1>
+  <p>Pagina responsive hecha con PHP Curl</p> 
 </div>
-  
 
 <div class="container">
+  <!-- Insertar Form -->
+  <form action="/insertar.php">
+    <div class="row">
+      <div class="col-sm-6">
+        <button type="submit" class="btn btn-primary">Insertar</button>
+      </div>
+    </div>
+  </form>
 
+  <!-- Borrar Form -->
+  <form action="/borrar.php">
+    <div class="row">
+      <div class="col-sm-6">
+        <button type="submit" class="btn btn-primary">Borrar</button>
+      </div>
+    </div>
+  </form>
 
-<form action="/insertar.php">
-<div class="row">
- <div class="col-sm-6" >
-  <button  type="submit" class="btn btn-primary">Insertar</button>
- </div>
- 
-</form>
-
-<form action="/borrar.php">
-
- 
-    <div class="col-sm-6">
-  <button type="submit" class="btn btn-primary">Borrar</button>
- </div>
-
-
-</form>
-
-<form action="/vertablas.php">
-
- 
-    <div class="col-sm-6">
-  <button type="submit" class="btn btn-primary">Ver tablas</button>
- </div>
-
-
-</form>
-
+  <!-- Ver tablas Form -->
+  <form action="/vertablas.php">
+    <div class="row">
+      <div class="col-sm-6">
+        <button type="submit" class="btn btn-primary">Ver tablas</button>
+      </div>
+    </div>
+  </form>
 </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
 <div class="container">
   <h2>Panel de control</h2>
 </div>
-
-
-
 
 <div class="container">
   <div class="row">
     <div class="col-sm-4">
       <h3>Insertar</h3>
-   
-<form  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Name: <input type="text" name="name">
-  
-  <br><br>
+      <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <!-- Name input -->
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" class="form-control" name="name" id="name">
+        </div>
 
-  E-mail: <input type="text" name="email">
+        <!-- Email input -->
+        <div class="form-group">
+          <label for="email">E-mail:</label>
+          <input type="text" class="form-control" name="email" id="email">
+        </div>
 
-  
-
-    <br><br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
-
-
-</div>
-<form  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-<div class="col-sm-4">
-      <h3>Borrar</h3>
- 
-   BORRAR_POR_ID: <input type="text" name="id">
-
-    <br><br>
-  <input type="submit" name="enviar" value="enviar">  
-</form>
+        <input type="submit" name="submit" value="Submit" class="btn btn-primary">  
+      </form>
     </div>
-   </div>
 
+    <div class="col-sm-4">
+      <h3>Borrar</h3>
+      <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <!-- ID input -->
+        <div class="form-group">
+          <label for="id">BORRAR_POR_ID:</label>
+          <input type="text" class="form-control" name="id" id="id">
+        </div>
+
+        <input type="submit" name="enviar" value="enviar" class="btn btn-primary">  
+      </form>
+    </div>
+  </div>
+</div>
 
 <?php
-
-//leer datos
-   $ch = curl_init();  
-   curl_setopt($ch, CURLOPT_URL, "localhost:8080/demo/all");  
-   curl_setopt($ch, CURLOPT_HEADER, false);  
-   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-   json_decode(('localhost:8080/demo/all'), true);  
-   $data = curl_exec($ch);  
- 
+// Leer datos
+$ch = curl_init();  
+curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/demo/all"); // Specify the correct URL
+curl_setopt($ch, CURLOPT_HEADER, false);  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+$data = curl_exec($ch);  
 $array = json_decode($data);
- // print_r($array);
- echo $array[0]->name;
 
-for($i=0;$i<count($array);$i++){ 
-
-	$id=$array[$i]->id;
-    $name = $array[$i]->name;
-    $email = $array[$i]->email;
-   // echo $id;
-   // echo $name;
-   // echo $email;
- 
-
-echo"<html>";
-echo"   <head>";
-echo"      <link href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css' rel='stylesheet'>";
-echo"   </head>";
-echo"   <body>";
-
-
-
-echo "<table id='lookup' class='table table-bordered table-hover'>  ";
-echo "	 <thead bgcolor='#eeeeee' align='center'> ";                
-                                echo "        <tr>";
-	  
-                                 echo "       <th>id</th>";
-	                              echo "      <th>name </th>";
-	                              echo "      <th>email </th>";
-
-	                              echo " </tr>";
-                                  echo "   </thead>";
-
-
-
-echo "<tr>"; 
-echo " <td>$id</td>";
-echo " <td>$name</td>";
-echo " <td>$email</td>";
-
- 
+// Display data in table
+echo "<table class='table table-bordered table-hover'>";  
+echo "<thead class='bg-light'>";                
+echo "<tr>";
+echo "<th>id</th>";
+echo "<th>name</th>";
+echo "<th>email</th>";
 echo "</tr>";
-echo "</table>";
+echo "</thead>";
 
+foreach ($array as $item) {
+  $id = $item->id;
+  $name = $item->name;
+  $email = $item->email;
 
-
-
-
-
-
-echo"      <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js'></script>";
-echo"   </body>";
-echo"</html>";
-
-
-
-
-
-
-
-
-
-
+  echo "<tr>"; 
+  echo "<td>$id</td>";
+  echo "<td>$name</td>";
+  echo "<td>$email</td>";
+  echo "</tr>";
 }
 
+echo "</table>";
 
+curl_close($ch);
 
+// Grabar datos
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  if (isset($_POST["submit"])) {
+    $postData = array(
+      "name" => $_POST["name"],
+      "email" => $_POST["email"]
+    );
 
-  curl_close($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/demo/add"); // Specify the correct URL
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec($ch);
+    print_r($data);
+    curl_close($ch);
+  }
 
+  if (isset($_POST["enviar"])) {
+    $id = $_POST["id"];
+    $url = "http://localhost:8080/demo/delete/$id";
 
-
-//grabar datos
- $postData = array( 
-   "name"=>$_POST["name"],
-   "email"=>$_POST["email"]
-
-   
-  );  
-  $ch = curl_init();  
-  curl_setopt($ch, CURLOPT_URL, "localhost:8080/demo/add");  
-  curl_setopt($ch, CURLOPT_HEADER, false);  
-  curl_setopt($ch, CURLOPT_POST, true);  
-  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));  
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
-  $data = curl_exec($ch);  
-  print_r($data);  
-  curl_close($ch);  
-
-
-
-
-
-//Borrar datos
-
-$ID = $_POST["id"];
-$ruta = "localhost:8080/demo/delete/";
-
-$nuevoStr = $ruta. $ID;
-
-
-   $ch = curl_init();  
-    curl_setopt($ch, CURLOPT_URL, $nuevoStr); 
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE"); 
-    curl_setopt($ch, CURLOPT_FAILONERROR, true);                                                                                                                    
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-     $data = curl_exec($ch);  
-      print_r($nuevoStr); 
-      curl_close($ch);  
-
-
-
-  ?>
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+    curl_setopt($ch, CURLOPT_FAILONERROR, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec($ch);
+    print_r($data);
+    curl_close($ch);
+  }
+}
+?>
 </body>
 </html>
