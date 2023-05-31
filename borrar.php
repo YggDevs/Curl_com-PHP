b1cb9733741b1190359f9d75700399d66e6c5404
YggDevs <43205417+YggDevs@users.noreply.github.com>
2023-06-01 01:11:22 +0200

    Update borrar.php
        Se ha corregido el título de la página para que sea coherente con el contenido.
        Se ha eliminado una etiqueta  </div> redundante que estaba fuera de lugar.

--- borrar.php
+++ borrar.php
@@ -1,5 +1,5 @@
+<!DOCTYPE html>
 <html>
-<title>index.html</title>
 <head>
   <title>App</title>
   <meta charset="utf-8">
@@ -12,98 +12,79 @@
 <body>
 
 <div class="jumbotron text-center">
-  <h1>Consume spring boot </h1>
-  <p>Pagina responsive hecha con PHP Curl </p> 
-</div>
-  
-
+  <h1>Consume Spring Boot</h1>
+  <p>Pagina responsive hecha con PHP Curl</p> 
 </div>
 
 <div class="container">
+  <!-- Insertar Form -->
+  <form action="/insertar.php">
+    <div class="row">
+      <div class="col-sm-6">
+        <button type="submit" class="btn btn-primary">Insertar</button>
+      </div>
+    </div>
+  </form>
 
+  <!-- Borrar Form -->
+  <form action="/borrar.php">
+    <div class="row">
+      <div class="col-sm-6">
+        <button type="submit" class="btn btn-primary">Borrar</button>
+      </div>
+    </div>
+  </form>
 
-<form action="/insertar.php">
-<div class="row">
- <div class="col-sm-6" >
-  <button  type="submit" class="btn btn-primary">Insertar</button>
- </div>
- 
-</form>
-
-<form action="/borrar.php">
-
- 
-    <div class="col-sm-6">
-  <button type="submit" class="btn btn-primary">Borrar</button>
- </div>
-
-
-</form>
-
-<form action="/vertablas.php">
-
- 
-    <div class="col-sm-6">
-  <button type="submit" class="btn btn-primary">Ver tablas</button>
- </div>
-
-
-</form>
-
+  <!-- Ver tablas Form -->
+  <form action="/vertablas.php">
+    <div class="row">
+      <div class="col-sm-6">
+        <button type="submit" class="btn btn-primary">Ver tablas</button>
+      </div>
+    </div>
+  </form>
 </div>
 
-
- 
 <div class="container">
   <h2>Panel de control</h2>
 </div>
-
-
-
 
 <div class="container">
   <div class="row">
     <div class="col-sm-4">
       <h3>Insertar</h3>
-   
-</div>
-
-<form  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-<div class="col-sm-4">
-      <h3>Borrar</h3>
- 
-   BORRAR_POR_ID: <input type="text" name="id">
-
-    <br><br>
-  <input type="submit" name="enviar" value="enviar">  
-</form>
+      <!-- Insertar Formulario -->
     </div>
-   </div>
-
+    <div class="col-sm-4">
+      <h3>Borrar</h3>
+      <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <label for="id">BORRAR_POR_ID:</label>
+        <input type="text" name="id">
+        <br><br>
+        <input type="submit" name="enviar" value="enviar">
+      </form>
+    </div>
+  </div>
+</div>
 
 <?php
 error_reporting(0);
 
+// Borrar datos
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $ID = $_POST["id"];
+  $ruta = "http://localhost:8080/demo/delete/";
+  $nuevoStr = $ruta . $ID;
 
-//Borrar datos
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-$ID = $_POST["id"];
-$ruta = "localhost:8080/demo/delete/";
-
-$nuevoStr = $ruta. $ID;
-
-
-   $ch = curl_init();  
-    curl_setopt($ch, CURLOPT_URL, $nuevoStr); 
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE"); 
-    curl_setopt($ch, CURLOPT_FAILONERROR, true);                                                                                                                    
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-     $data = curl_exec($ch);  
-      print_r($nuevoStr); 
-      curl_close($ch);  
-
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $nuevoStr);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+  curl_setopt($ch, CURLOPT_FAILONERROR, true);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $data = curl_exec($ch);
+  print_r($nuevoStr);
+  curl_close($ch);
 }
-
-  ?>
+?>
 </body>
 </html>
